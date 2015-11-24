106c5cc4bcab971c53ef36c407813ceb18bfe98b
Nathan Pemberton <nathanp@berkeley.edu>
2015-11-24 11:09:18 -0800

    Finished TLB test

--- basis_bench/tlb.c
+++ basis_bench/tlb.c
@@ -1,13 +1,48 @@
+/* A benchmark intended to stress the TLB and DRAM.
+   Reads the first cache line from every page in random order
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <assert.h>
+/* Size of test array (defines problem size, must be divisible by PG_SZ)*/
+#define ARR_SZ (4*1024L*1024L*1024L)
 
-//#define ITERATIONS 1024*1024
-#define ARR_SZ 1024*1024*1024
+/* Size of a page (XXX should really use something more portable) */
+#define PG_SZ (4*1024)
+
+#define NPAGE (ARR_SZ / PG_SZ)
+#define ITERATIONS (NPAGE*128)
+
+/* Size of an L1 d-cache line */
+#define LN_SZ 64
+
+static inline uint64_t fast_rand(void) {
+  static uint64_t rand = 0xDEADBEEF;
+  uint64_t bit = 0;
+
+  bit  = ((rand >> 0) ^ (rand >> 1) ^ (rand >> 3) ^ (rand >> 4) ) & 1;
+  rand =  (rand >> 1) | (bit << 63);
+
+  return rand;
+}
 
 int main(int argc, char *argv[]) {
-  char *arr = malloc(ARR_SZ);
+  uint8_t *arr = malloc(ARR_SZ);
+  assert(arr != NULL);
+  uint8_t res = 0;
 
-  //Access the array in a page-aligned strided manner
+  // Multiple iterations
+  for(int64_t ix = 0; ix < ITERATIONS; ix++) {
+    /* Read a random page */
+    uint8_t *page = &arr[ (fast_rand() % NPAGE ) * PG_SZ ];
+    for(int64_t lnx = 0; lnx < LN_SZ; lnx++) {
+      /* Just read the first cache-line from the page to boost cache-hits */
+      res += page[lnx];
+    }
+  }
+  volatile uint8_t trick = res;
+  printf("Done\n");
 
+  return EXIT_SUCCESS;
 }
